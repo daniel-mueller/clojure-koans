af03f37bf8f73b9da58fb5302e5b6d7e52a5aae8
Daniel K Mueller <danielmueller@limebird.de>
2014-09-20 14:26:59 +0200

    finished

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -7,14 +7,22 @@
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         reversed '()]
+    (if (empty? coll)
+      reversed
+      (recur (rest coll) (cons (first coll) reversed)))))
 
 (defn factorial [n]
-  __)
+  (loop[n n
+        acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
